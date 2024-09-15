7954e541590f0ec3f150c82584f20fe877f98b29
Darnell <darnellsylvain@googlemail.com>
2024-09-15 12:00:25 +0100

    started extract images func

---
+++ src/inline_markdown.py
@@ -0,0 +1,38 @@
+import re
+
+from textnode import TextNode, text_type_text, text_type_code
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if not isinstance(node, TextNode):
+            raise ValueError("Invalid node type - must be TextNode")
+
+        if node.text_type != text_type_text:
+            new_nodes.extend([node])
+            continue
+
+        split_nodes = []
+        sections = node.text.split(delimiter)
+
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], text_type_text))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+
+
+def extract_markdown_images(text):
+    image_regex = r"!\[(.*?)\]\((.*?)\)"
+    alt = re.findall(image_regex, text)
+
+    print(f"ALT: {alt}")
+    return alt
--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from textnode import TextNode
+from inline_markdown import extract_markdown_images
 
 
 def main():
     textNode = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(textNode)
+    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    extract_markdown_images(text)
 
 
 main()
